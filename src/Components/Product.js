95771efc608ccd1157c2870c876a22ec30c38183
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-29 14:44:29 +0800

    Fetch all Products

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import axios from 'axios';
 
 function ProductList() {
@@ -16,9 +22,31 @@ function ProductList() {
 
     return (
         <div>
-            <h2>Product List</h2>
             {products.map((product) => (
-                <li key={product._id}>{product.name}</li>
+                <Card sx={{ maxWidth: 345, margin: '10px' }} >
+                    <CardMedia
+                        sx={{ height: 200 }}
+                        image={product.images[0].url}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {product.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'left' }}>
+                            Colorway: {product.colorway}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'left' }}>
+                            Brand: {product.brand}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'left' }}>
+                            Size: {product.size}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Add to cart</Button>
+                        <Button size="small">Details</Button>
+                    </CardActions>
+                </Card>
             ))}
         </div>
     );
