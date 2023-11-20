59c840bdfbea18eb958070f5c08b9bb1e1d4be5d
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-11-20 10:40:42 +0800

    Per page display

--- src/Components/Admin/OrdersList.js
+++ src/Components/Admin/OrdersList.js
@@ -287,10 +287,10 @@ const OrdersList = () => {
                             columns={setOrders().columns}
                             initialState={{
                                 pagination: {
-                                    paginationModel: { page: 0, pageSize: 5 },
+                                    paginationModel: { page: 0, pageSize: 10 },
                                 },
                             }}
-                            pageSizeOptions={[5, 10]}
+                            pageSizeOptions={[10, 20]}
                         />
                     </div>
                 </Box>
--- src/Components/Admin/ProductList.js
+++ src/Components/Admin/ProductList.js
@@ -299,10 +299,10 @@ const ProductList = () => {
                             columns={productsList().columns}
                             initialState={{
                                 pagination: {
-                                    paginationModel: { page: 0, pageSize: 5 },
+                                    paginationModel: { page: 0, pageSize: 10 },
                                 },
                             }}
-                            pageSizeOptions={[5, 10]}
+                            pageSizeOptions={[10, 20]}
                         />
                     </div>
                 </Box>
